69dcb7baff8cf6e1115c7921e3d3f3dfc8ee0851
Morgan Houppin <morganhouppin@gmail.com>
2022-06-28 10:55:07 +0200

    Updated build script + .gitignore

--- utils/build.sh
+++ utils/build.sh
@@ -1,10 +1,21 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+set -e
+
+cd -- "$( dirname -- "${BASH_SOURCE[0]}" )"
+cd ..
+
+git lfs fetch origin main
 
 cd src
 
 CFLAGS="$CFLAGS -fprofile-generate" LDFLAGS="$LDFLAGS -lgcov" ARCH="$ARCH" make re
 
-./stash-bot bench
+./vault "setoption name EvalFile value ../default.nn" bench
 
 CFLAGS="$CFLAGS -fprofile-use -fno-peel-loops -fno-tracer" LDFLAGS="$LDFLAGS -lgcov" \
     ARCH="$ARCH" make re
+
+make clean
+
+rm src/sources/*.gcda
